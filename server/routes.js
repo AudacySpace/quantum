11b983a81cc2467a0accb2e8fa17cced82cec491
Chavi Malhotra <chavi.malhotra@gmail.com>
2017-12-04 12:10:26 -0800

    updated routes

--- server/routes.js
+++ server/routes.js
@@ -2,9 +2,6 @@ module.exports = function(app) {
 
 	// application -------------------------------------------------------------
 	app.get('*', function(req, res) {
-		res.sendFile(__base + '/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-
-		// res.sendFile('index.html', { root: __dirname + '' });
-
+		res.sendFile(__base + '/index.html');
 	});
 };
