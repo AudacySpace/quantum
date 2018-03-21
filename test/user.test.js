984d722fd94d5ddf478650dfddaeba0545657570
tarunigattu1992 <taruni.gattu@audacy.space>
2018-03-21 16:34:04 -0700

    commiting user model get route controller unit test cases

--- test/user.test.js
+++ test/user.test.js
@@ -142,3 +142,188 @@ describe('Test Suite for User Model ', function() {
     });
 
 });
+
+describe('Test Suite for User Model Route Controller', function() {
+    beforeEach(function() {
+        sinon.stub(Usr, 'find');
+        sinon.stub(Usr,'findOne');
+    });
+ 
+ 
+    afterEach(function() {
+        Usr.find.restore();
+        Usr.findOne.restore();
+    });
+ 
+    it('should get current role of the user', function() {
+        userCtrl = require('../server/controllers/user.controller');
+        var error = null;
+        var user = {
+                google:{},
+                missions:[
+                {
+                    name:"AZero",
+                    currentRole:'MD',
+                    allowedRoles:[
+                        {callsign:'SYS'},
+                        {callsign:'CC'}
+                    ]
+                }]
+            }
+        Usr.findOne.yields(error, user);
+        var req = {
+            query : {
+                mission:'AZero',
+                email:'tgattu@gmail.com'
+            }
+        }
+        var res = {
+            send: sinon.spy()
+        }
+
+        userCtrl.getCurrentRole(req, res);
+        sinon.assert.calledWith(Usr.findOne,{ 'google.email' : 'tgattu@gmail.com', 'missions.name' : 'AZero' },{'missions.$' : 1 },sinon.match.func);
+        expect(res.send.calledOnce).to.be.true;
+        sinon.assert.calledWith(res.send, 'MD');
+    });
+
+    it('should not get current role of the user when error', function() {
+        userCtrl = require('../server/controllers/user.controller');
+        var error = {name:"MongoError"};
+        var user = null;
+        Usr.findOne.yields(error,user);
+        var req = {
+            query : {
+                mission:'AZero',
+                email:'tgattu@gmail.com'
+            }
+        }
+        var res = {
+            send: sinon.spy()
+        }
+
+        userCtrl.getCurrentRole(req, res);
+        sinon.assert.calledWith(Usr.findOne,{ 'google.email' : 'tgattu@gmail.com', 'missions.name' : 'AZero' },{'missions.$' : 1 },sinon.match.func);
+        expect(res.send.calledOnce).to.be.false;
+    });
+
+    it('should get allowed roles of the user', function() {
+        userCtrl = require('../server/controllers/user.controller');
+        var error = null;
+        var user = {
+                google:{},
+                missions:[
+                {
+                    name:"AZero",
+                    currentRole:'MD',
+                    allowedRoles:[
+                        {callsign:'SYS'},
+                        {callsign:'CC'}
+                    ]
+                }]
+            }
+        Usr.findOne.yields(error, user);
+        var req = {
+            query : {
+                mission:'AZero',
+                email:'tgattu@gmail.com'
+            }
+        }
+        var res = {
+            send: sinon.spy()
+        }
+
+        userCtrl.getAllowedRoles(req, res);
+        sinon.assert.calledWith(Usr.findOne,{ 'google.email' : 'tgattu@gmail.com', 'missions.name' : 'AZero' },{'missions.$' : 1 },sinon.match.func);
+        expect(res.send.calledOnce).to.be.true;
+        sinon.assert.calledWith(res.send, [{callsign:'SYS'},{callsign:'CC'}]);
+    });
+
+    it('should not get allowed roles of the user when error', function() {
+        userCtrl = require('../server/controllers/user.controller');
+        var error = {name:"MongoError"};
+        var user = null;
+        Usr.findOne.yields(error,user);
+        var req = {
+            query : {
+                mission:'AZero',
+                email:'tgattu@gmail.com'
+            }
+        }
+        var res = {
+            send: sinon.spy()
+        }
+
+        userCtrl.getAllowedRoles(req, res);
+        sinon.assert.calledWith(Usr.findOne,{ 'google.email' : 'tgattu@gmail.com', 'missions.name' : 'AZero' },{'missions.$' : 1 },sinon.match.func);
+        expect(res.send.calledOnce).to.be.false;
+    });
+
+    it('should get all users', function() {
+        userCtrl = require('../server/controllers/user.controller');
+        var error = null;
+        var users = [
+                {
+                    google:{},
+                    missions:[
+                        {
+                            name:"AZero",
+                            currentRole:'MD',
+                            allowedRoles:[
+                                {callsign:'SYS'},
+                                {callsign:'CC'}
+                            ]
+                        }
+                    ]
+                }
+                            
+            ];
+        Usr.find.yields(error, users);
+        var req = {
+            query : {
+                mission:'AZero'
+            }
+        }
+        var res = {
+            send: sinon.spy()
+        }
+
+        userCtrl.getUsers(req, res);
+        sinon.assert.calledWith(Usr.find,{ 'missions.name' : 'AZero' },{'google' : 1, 'missions.$' : 1 },sinon.match.func);
+        expect(res.send.calledOnce).to.be.true;
+        sinon.assert.calledWith(res.send, [{ allowedRoles: { CC: 1, SYS: 1 }, currentRole: "MD", google: {  } }]);
+    });
+
+    it('should not get all users when error', function() {
+        userCtrl = require('../server/controllers/user.controller');
+        var error = {name:"MongoError"};
+        var users = null;
+        Usr.find.yields(error,users);
+        var req = {
+            query : {
+                mission:'AZero'
+            }
+        }
+        var res = {
+            send: sinon.spy()
+        }
+
+        userCtrl.getUsers(req, res);
+        sinon.assert.calledWith(Usr.find,{ 'missions.name' : 'AZero' },{'google' : 1, 'missions.$' : 1 },sinon.match.func);
+        expect(res.send.calledOnce).to.be.false;
+    });
+
+    it('should get all roles', function() {
+        userCtrl = require('../server/controllers/user.controller');
+        var req = {}
+        var res = {
+            send: sinon.spy()
+        }
+        var output = require('../config/role');
+
+        userCtrl.getRoles(req, res);
+        expect(res.send.calledOnce).to.be.true;
+        sinon.assert.calledWith(res.send, output);
+    });
+
+});
